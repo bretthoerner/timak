352259a6ca5ccf68a4889723f5e6f344e744aca2
Brett Hoerner <brett@bretthoerner.com>
2011-08-06 14:32:25 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='timak',
+    version=".".join(map(str, __import__("timak").__version__)),
+    description='Timelines in by Riak',
+    author='Brett Hoerner',
+    author_email='brett@bretthoerner.com',
+    url='http://github.com/bretthoerner/timak',
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    license="Apache License (2.0)",
+)
